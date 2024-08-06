4cec3471b6649c06b7914fc419b1e2a4ba77ef08
Harmeet <hs978698@gmail.com>
2024-08-06 21:50:05 +0530

    potd 3016

---
+++ MinPushesNumber.cpp
@@ -0,0 +1,20 @@
+//Leetcode problem: 3016. Minimum Number of Pushes to Type Word II
+//Link: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/
+class Solution {
+public:
+    int minimumPushes(string word) {
+        
+        //Generic solution
+        vector<int> mp(26, 0);
+        for(char &ch: word)
+            mp[ch - 'a']++; //Mentioned in qn, all letters will be distinct
+    
+        sort(begin(mp), end(mp), greater<int>()); //descending order of frequency
+        
+        int ans = 0;
+        for(int i = 0; i < 26; i++){
+            ans += mp[i]*((i/8) + 1);
+        }
+        return ans;
+    }
+};
