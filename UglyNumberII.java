c980c931c3f1f4d392e4793d0776981ad9e100b1
Harmeet <hs978698@gmail.com>
2024-08-18 23:38:02 +0530

    ugly number Potd

---
+++ UglyNumberII.java
@@ -0,0 +1,28 @@
+//Leetcode Problem Number: 264 Ugly Number II
+public class UglyNumberII {
+    public int nthUglyNumber(int n) {
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        int nextMultipleOf2 = 2;
+        int nextMultipleOf3 = 3;
+        int nextMultipleOf5 = 5;
+        for (int i = 1; i < n; i++) {
+            int nextUglyNumber = Math.min(nextMultipleOf2, Math.min(nextMultipleOf3, nextMultipleOf5));
+            ugly[i] = nextUglyNumber;
+            if (nextUglyNumber == nextMultipleOf2) {
+                i2++;
+                nextMultipleOf2 = ugly[i2] * 2;
+            }
+            if (nextUglyNumber == nextMultipleOf3) {
+                i3++;
+                nextMultipleOf3 = ugly[i3] * 3;
+            }
+            if (nextUglyNumber == nextMultipleOf5) {
+                i5++;
+                nextMultipleOf5 = ugly[i5] * 5;
+            }
+        }
+        return ugly[n - 1];
+    }
+}
