6450aa4a90bef8a777e5cee431b4a23a3b17adb8
Harmeet <hs978698@gmail.com>
2024-08-23 00:13:33 +0530

    potd

---
+++ NumberComplement.java
@@ -0,0 +1,10 @@
+//Leetcode Problem 476. Number Complement
+public class NumberComplement {
+    public int findComplement(int num) {
+        int n = 0;
+        while (n < num) {
+            n = (n << 1) | 1;
+        }
+        return n - num;
+    }   
+}
