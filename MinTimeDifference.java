61906cc79101ebdc21a21bb47fb151b46a0aaf4e
Harmeet <hs978698@gmail.com>
2024-09-16 23:00:05 +0530

    potd ltc

---
+++ MinTimeDifference.java
@@ -0,0 +1,18 @@
+// Leetcode Probem 539. Minimum Time Difference
+public class MinTimeDifference {
+    public int findMinDifference(List<String> timePoints) {
+        int n = timePoints.size();
+        int[] minutes = new int[n];
+        for (int i = 0; i < n; i++) {
+            String[] time = timePoints.get(i).split(":");
+            minutes[i] = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
+        }
+        Arrays.sort(minutes);
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            minDiff = Math.min(minDiff, minutes[i] - minutes[i - 1]);
+        }
+        minDiff = Math.min(minDiff, minutes[0] + 1440 - minutes[n - 1]);
+        return minDiff;
+    }   
+}
